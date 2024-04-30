8453e87cc2064c8e88615584ae65a7883379c605
shadow0143 <belal8340325636s@gmail.com>
2024-04-30 15:22:20 +0530

    gif changes

--- js/pics2gif.js
+++ js/pics2gif.js
@@ -2,11 +2,11 @@
 
 // load svg file
 function previewFiles() {
-  var files   = openfile.files;
-  
+  var files = openfile.files;
+
   function loadfile(file) {
     var reader = new FileReader();
-    reader.onload = function(e) {
+    reader.onload = function (e) {
       var img = new Image();
       img.src = e.target.result;
       img.classList.add("pointer");
@@ -20,28 +20,28 @@ function previewFiles() {
     };
     reader.readAsDataURL(file);
   }
-  
+
   if (files) {
     [].forEach.call(files, loadfile);
   }
 };
 function dropfile(event) {
-  var dt    = event.dataTransfer;
+  var dt = event.dataTransfer;
   var files = dt.files;
   var count = files.length;
-  
+
   for (var i = 0; i < files.length; i++) {
     if (files[i].type === "image/svg+xml") {
       alertify.alert("Sorry SVG files are not allowed. Please convert it to a PNG!").set("basic", true);;
       return false;
     }
-    
+
     var reader = new FileReader();
-    reader.onload = function(e) {
+    reader.onload = function (e) {
       var img = new Image();
       img.classList.add("pointer");
       img.setAttribute('onclick', "removeElm(this);");
-      img.src        = e.target.result;
+      img.src = e.target.result;
       dimensions.src = e.target.result;
       animWidth.value = dimensions.width;
       animHeight.value = dimensions.height;
@@ -53,21 +53,23 @@ function dropfile(event) {
   }
 };
 
-openfile.onchange = function() {
+openfile.onchange = function () {
   previewFiles();
-  
+  const selectMessage = document.getElementById('selectMessage');
+  selectMessage.style = "display:none"
+
   dropflash.classList.remove("hide");
   $("#dropflash").fadeOut();
 };
-read.ondragover   = function(e) {
+read.ondragover = function (e) {
   this.style.opacity = ".5";
   return false;
 };
-read.ondragend    = function() {
+read.ondragend = function () {
   read.style.opacity = "1";
   return false;
 };
-read.ondrop       = function(e) {
+read.ondrop = function (e) {
   e.preventDefault();
   read.style.opacity = "1";
   dropflash.classList.remove("hide");
@@ -81,31 +83,36 @@ function removeElm(elm) {
 }
 
 // create the gif animation
-creategif.onclick = function() {
+creategif.onclick = function () {
   // hide elements
   read.classList.add("hide");
   scripttxt.classList.add("hide");
   creategif.classList.add("hide");
-  
+
   // show process and then display result
   showprocess.classList.remove("hide");
   var images = [];
-  $("#imgframes img").each(function() {
+  $("#imgframes img").each(function () {
     images.push($(this).attr("src"));
     $(this).removeAttr("onclick");
     $(this).removeClass("pointer");
   });
-  
+
   gifshot.createGIF({
     images: images,
     gifWidth: animWidth.value,
     gifHeight: animHeight.value,
     interval: animRate.value, // seconds
-    progressCallback: function(captureProgress) { console.log('progress: ', captureProgress); },
-    completeCallback: function() { console.log('completed!!!'); },
+    progressCallback: function (captureProgress) {
+      console.log('progress: ', captureProgress); 
+    },
+    completeCallback: function () { 
+      console.log('completed!!!'); 
+    },
     numWorkers: 2,
-  },function(obj) {
-    if(!obj.error) {
+  }, function (obj) {
+    console.log('obj ======================', obj);
+    if (!obj.error) {
       var image = obj.image;
       result.src = image;
       showit.classList.remove("hide");
@@ -116,20 +123,20 @@ creategif.onclick = function() {
 };
 
 // export gif animation
-exportgif.onclick = function() {
+exportgif.onclick = function () {
   this.href = result.src;
 };
 
 // initiate animation when values change
-animRate.style.width    = ((animRate.value.length + 1) * 30) + "px";
-animWidth.style.width   = ((animWidth.value.length + 1) * 30) + "px";
-animHeight.style.width  = ((animHeight.value.length + 1) * 30) + "px";
-animRate.onkeydown    = function(e) {
-  this.style.width  = ((this.value.length + 1) * 22) + "px";
+animRate.style.width = ((animRate.value.length + 1) * 30) + "px";
+animWidth.style.width = ((animWidth.value.length + 1) * 30) + "px";
+animHeight.style.width = ((animHeight.value.length + 1) * 30) + "px";
+animRate.onkeydown = function (e) {
+  this.style.width = ((this.value.length + 1) * 22) + "px";
 }
-animWidth.onkeydown   = function(e) {
-  this.style.width  = ((this.value.length + 1) * 22) + "px";
+animWidth.onkeydown = function (e) {
+  this.style.width = ((this.value.length + 1) * 22) + "px";
 }
-animHeight.onkeydown  = function(e) {
-  this.style.width  = ((this.value.length + 1) * 22) + "px";
+animHeight.onkeydown = function (e) {
+  this.style.width = ((this.value.length + 1) * 22) + "px";
 }
